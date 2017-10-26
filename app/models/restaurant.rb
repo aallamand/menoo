ab5d7a516809ba586e0257558b9b12983b6f91cc
lucas steuperaert <lucas.steuperaert@gmail.com>
2017-10-26 14:21:15 +0200

    install algolia api

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,9 @@
 class Restaurant < ApplicationRecord
   has_many :menus, dependent: :nullify
+  include AlgoliaSearch
+
+  algoliasearch do
+    attribute :name
+  end
 end
+
---
+++ config/initializers/algoliasearch.rb
@@ -0,0 +1,4 @@
+AlgoliaSearch.configuration = {
+  application_id: 'X7APVCWJ0V',
+  api_key: '1ba617844509637d0c2c55d1368a0888'
+}
