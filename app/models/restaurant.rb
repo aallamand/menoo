9643197601dbacd264100a51b4d0c487c39a4159
Arthur Allamand <a.allamand@gmail.com>
2017-10-27 11:19:40 +0200

    dev is no longer impacting prod indices

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ApplicationRecord
   has_many :menus, dependent: :nullify
   include AlgoliaSearch
 
-  algoliasearch do
+  algoliasearch per_environment: true, disable_indexing: Rails.env.test? do
     attribute :name, :address
   end
 end
